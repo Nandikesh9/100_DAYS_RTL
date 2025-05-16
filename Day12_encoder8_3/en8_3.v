d0042d01b89cf28cfbd825a0419d1786c6f454a6
Venkata Naga Nandikesh Kalepu <84128665+Nandikesh9@users.noreply.github.com>
2025-05-16 19:03:37 +0530

    Update en8_3.v

--- Day12_encoder8_3/en8_3.v
+++ Day12_encoder8_3/en8_3.v
@@ -19,28 +19,41 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module en8_3(
+module en_8_3_4_2(
     input [7:0] i,
-    output reg [2:0] y,
-    input enb
+    output reg [2:0] y
     );
+	 wire [1:0]y0,y1;
+
+	 en4_2 en0(.a(i[3:0]), .enb(i[3:0]!=4'b0000), .y(y0)); //i[3:0]!=4'b0000 enables when any lower bit is high
+	 en4_2 en1(.a(i[7:4]), .enb(i[7:4]!=4'b0000), .y(y1)); //i[7:4]!=4'b0000 enables when any lower  bit is high
 	 always@(*) begin
+		if(i[7:4]!=4'b0000) begin
+			y[2]=1'b1;
+			y[1:0]=y1;
+		end
+		else	begin
+			y[2]=1'b0;
+			y[1:0]=y0;
+		end
+	end
+endmodule
+// **code for 4:2 encoder**
+module en4_2(
+    input [3:0] a,
+	 input enb,
+    output reg [1:0] y
+    );
+	 always @(*) begin
 		if(enb)
-			case(i)
-				8'b0000_0001: y=3'b000;
-				8'b0000_0010: y=3'b001;
-				8'b0000_0100: y=3'b010;
-				8'b0000_1000: y=3'b011;
-				8'b0001_0000: y=3'b100;
-				8'b0010_0000: y=3'b101;
-				8'b0100_0000: y=3'b110;
-				8'b1000_0000: y=3'b111;
-				default: y=3'b000;
+			case(a)
+				 4'b0001: y=2'b00;
+				 4'b0010: y=2'b01;
+				 4'b0100: y=2'b10;
+				 4'b1000: y=2'b11;
+				 default :y=2'b00;
 			endcase
 		else
-			y = 3'b000;
-	 end
-	 
-
-
+			y=2'b00;
+	  end
 endmodule
